f497c5b7306487e8755dbfe7c0331b9ae11bf620
Altan Emre <altan_emre_1989@hotmail.com>
2025-03-18 21:46:19 +0300

    ViewComponent

---
+++ 05_Layout_PartialView_RenderSection/ViewComponents/_PartialBrandViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using _05_Layout_PartialView_RenderSection.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _05_Layout_PartialView_RenderSection.ViewComponents
+{
+	public class _PartialBrandViewComponent:ViewComponent
+	{
+		DataContext db = new DataContext();
+
+
+		public IViewComponentResult Invoke()
+		{
+			var brands = db.Brands.ToList();
+			return View(brands);
+		}
+	}
+}
